9ff7c1de7f6de1ac283ba009fd15f414d31a9a1d
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2023-01-11 15:40:29 +0100

    loading recipe sa API, async funkcija, catch error, poruka error

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -539,8 +539,20 @@ const timeout = function(s) {
             reject(new Error(`Request took too long! Timeout after ${s} second`));
         }, s * 1000);
     });
-}; // https://forkify-api.herokuapp.com/v2
- ///////////////////////////////////////
+};
+// https://forkify-api.herokuapp.com/v2
+///////////////////////////////////////
+const showRecipe = async function() {
+    try {
+        const res = await fetch("https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886");
+        const data = await res.json();
+        if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+        console.log(res, data);
+    } catch (err) {
+        alert(err);
+    }
+};
+showRecipe();
 
 },{}]},["fA0o9","aenu9"], "aenu9", "parcelRequire3a11")
 
